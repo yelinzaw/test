433b22fa5d4860c2e0c95677d11d29e5631e0860
yelinzaw <yelinn.info@gmail.com>
2015-06-24 18:40:15 +0630

    adding 1500.cpp

---
+++ 1500.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+int main()
+{
+	int x,y,z;
+	cout<<"1St Number is ";
+	cin>>x;
+	cout<<"Enter Second Number ";
+	cin>>y;
+	z=x+y;
+	if (z==1500)
+	cout<<" \n"<<"I Love You. \n"<<"You are my love."<<" \n"<<endl;
+	else
+	cout<<"Try Again"<<endl;
+}
