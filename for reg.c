db23792a5db109994e9c2c971a53e0c95ad2b8db
yelinzaw <yelinn.info@gmail.com>
2015-07-15 14:49:55 +0630

    adding reg.c

---
+++ for reg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+
+{
+
+
+
+    printf("Hello World \n\n\n");
+    system("echo Hey your computer is -===HACKED===- ");
+    system("\n\nPAUSE \n");
+    return 0;
+
+}
